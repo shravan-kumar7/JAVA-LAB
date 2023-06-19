661efabdefee6b0f5f6e3416b6ce58feaed246f5
shravan-kumar7 <121107760+shravan-kumar7@users.noreply.github.com>
2023-06-19 19:24:47 +0530

    method overloading program

---
+++ 08_methodoverload.java
@@ -0,0 +1,13 @@
+	class Adder
+  {  
+	  static int add(int a,int b)  {return a+b;}  
+	  static int add(int a,int b,int c)  {return a+b+c;}  
+	}  
+	class overloading
+  {  
+    public static void main(String[] args)
+      {  
+        System.out.println(Adder. Add(11,11));  
+        System.out.println(Adder.add(11,11,11));  
+      }
+  } 
