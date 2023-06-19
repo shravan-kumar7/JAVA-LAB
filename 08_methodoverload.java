cafb7a4a2c8836bf71c8f52ff4df297a007fa9bc
shravan-kumar7 <121107760+shravan-kumar7@users.noreply.github.com>
2023-06-19 19:30:05 +0530

    method overloading program

--- 08_methodoverload.java
+++ 08_methodoverload.java
@@ -7,7 +7,7 @@
   {  
     public static void main(String[] args)
       {  
-        System.out.println(Adder. Add(11,11));  
+        System.out.println(Adder.add(11,11));  
         System.out.println(Adder.add(11,11,11));  
       }
   } 
