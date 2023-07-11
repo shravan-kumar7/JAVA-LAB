aa1e9122b59ec2a9b353eceb3910a63cd2ded7b1
shravan-kumar7 <121107760+shravan-kumar7@users.noreply.github.com>
2023-07-11 09:35:22 +0530

    multiple inheritance

---
+++ 09_multipleinheritance.java
@@ -0,0 +1,32 @@
+package is122;
+
+import java.util.*;
+import java.io.*;
+interface one{
+void sum(int a,int b);
+}
+interface two{
+void difference(int a,int b);
+}
+class ABC implements one,two{
+public void sum(int a,int b) {
+int sum=a+b;
+System.out.println("sum of"+a+"and"+b+"is:"+sum);
+}
+public void difference(int a,int b) {
+int difference=a-b;
+System.out.println("Difference of"+a+"and"+b+"is:"+difference);
+}
+}
+class multipleinheritance{
+public static void main(String []args) {
+ABC obj=new ABC();
+Scanner s=new Scanner(System.in);
+int a,b;
+System.out.println("enter the values of a and b:");
+a=s.nextInt();
+b=s.nextInt();
+obj.sum(a,b);
+obj.difference(a,b);
+}
+}
